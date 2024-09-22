1370bd89ac07c5aac4855aba75ae832cf7af448b
Lucas Marcial <lucasmarcial.m12@gmail.com>
2024-09-22 13:04:13 -0300

    agregados todos los templates individuales y renderizados

--- app_coder/urls.py
+++ app_coder/urls.py
@@ -4,7 +4,6 @@ from django.urls import path
 from app_coder.views import crea_curso, lista_cursos, estudiantes, entregables, profesores, inicio  
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('crea_curso/<nombre>/<camada>', crea_curso),
     path('lista_curso/', lista_cursos),
     path('profesores/', profesores),
