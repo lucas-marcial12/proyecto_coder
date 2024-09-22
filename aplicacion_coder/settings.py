3f7d6e7c09a01fd625643ba6da767b6a59f5c0d4
Lucas Marcial <lucasmarcial.m12@gmail.com>
2024-09-22 09:38:20 -0300

    agregar template

--- aplicacion_coder/settings.py
+++ aplicacion_coder/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'aplicacion_coder.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [f'{BASE_DIR}/aplicacion_coder/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
